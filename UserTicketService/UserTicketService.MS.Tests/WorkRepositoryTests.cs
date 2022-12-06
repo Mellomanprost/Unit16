12cf451f301836558bccddf3f995ede9bb76e40d
Mellomanprost <anoppa@mail.ru>
2022-12-06 21:13:04 +0300

    added strange test

--- UserTicketService/UserTicketService.MS.Tests/WorkRepositoryTests.cs
+++ UserTicketService/UserTicketService.MS.Tests/WorkRepositoryTests.cs
@@ -18,5 +18,24 @@ namespace UserTicketService.MS.Tests
 
             Assert.AreEqual(true, privateObject.Invoke("ConnectionTrue"));
         }
+
+        //[TestMethod]
+        //public void SaveTicket()
+        //{
+        //    var ticketServiceTest = new TicketService();
+        //    var newTicket = new Ticket(300, "Test ticket", 1000);
+
+        //    ticketServiceTest.SaveTicket(newTicket);
+
+        //    var allTicketsAfterAddingNewTicket = ticketServiceTest.GetAllTickets();
+        //    CollectionAssert.Contains(allTicketsAfterAddingNewTicket, newTicket);
+
+        //    PrivateObject obj = new PrivateObject(ticketServiceTest);
+
+        //    obj.Invoke("DeleteTicket", newTicket);
+
+        //    var allTicketsAfterDeletingNewTicket = ticketServiceTest.GetAllTickets();
+        //    CollectionAssert.DoesNotContain(allTicketsAfterDeletingNewTicket, newTicket);
+        //}
     }
 }
